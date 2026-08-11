d9eeb3312052c9446e4cbb5f9e332527f14b843f
Aryan Kumar <aryanempire168@gmail.com>
2026-08-11 23:15:28 +0530

    Time: 58 ms (19.71%) | Memory: 8.1 MB (26.83%) - LeetSync

---
+++ 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        priority_queue<int>pq;
+        pq.push(a);
+        pq.push(b);
+        pq.push(c);
+
+        int cnt = 0;
+        while(pq.size() > 1){
+            cnt++;
+            int first = pq.top();
+            pq.pop();
+            int second = pq.top();
+            pq.pop();
+        
+            first--;
+            second--;
+
+            if(first > 0)pq.push(first);
+            if(second > 0)pq.push(second);
+
+
+        }
+        return cnt;
+        
+    }
+};
