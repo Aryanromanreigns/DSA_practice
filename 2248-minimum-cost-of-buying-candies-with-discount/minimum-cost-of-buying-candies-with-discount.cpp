670813fcfea19039b8073638a2cadddc7447170c
Aryan Kumar <aryanempire168@gmail.com>
2026-08-19 18:15:41 +0530

    Time: 4 ms (5.24%) | Memory: 14.2 MB (53.19%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        int n = cost.size();
+        sort(cost.begin() , cost.end());
+        int i = n - 1;
+        
+        int sum = 0;
+        while(i >= 0){
+
+            sum += cost[i];
+            i--;
+            if(i >= 0)
+            sum += cost[i];
+            i--;
+            if(i >=0 ){
+                i--;
+            }
+
+        }
+        return sum;
+        
+    }
+};
