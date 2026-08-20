4a44bcac9b58e24f4ae43acec6a01addbfd338f7
Aryan Kumar <aryanempire168@gmail.com>
2026-08-21 00:41:49 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (77.08%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int m = flowerbed.size();
+    bool canPlaceFlowers(vector<int>& arr, int n) {
+        int m = arr.size();
         if(n == 0)return true;
-        if(m == 1) {
-            if(flowerbed[0] == 0) n--;
-            return n == 0;
+
+        if(m == 1){
+            if(arr[0] == 0){
+                n--;
+                if(n == 0)return true;
+                return false;
+            }
+            return false;
         }
-        if(flowerbed[0] == 0 && flowerbed[1] == 0){
-            flowerbed[0] = 1;
+        if(arr[0] == 0 && arr[1] == 0){
+            arr[0] = 1;
             n--;
             if(n == 0)return true;
         }
-        for(int i = 1 ; i < m-1 ; i++){
-            if(flowerbed[i] == 0 && flowerbed[i-1] == 0 &&
-                 flowerbed[i+1] == 0){
-                    flowerbed[i] = 1;
+        int i = 1;
+        for(i = 1 ; i < m-1 ; i++)
+        {
+            if(arr[i] == 0 && arr[i-1] == 0 && arr[i+1] == 0 ){
+                arr[i] = 1;
                 n--;
                 i++;
-                if(n == 0)return true;
+            }
+            if(n == 0)return true;
 
+        }
+        if(n > 0){
+            if(arr[m-2] == 0 && arr[m-1] == 0){
+                arr[m-1] = 1;
+                n--;
             }
         }
-        if(flowerbed[m-1] == 0 && flowerbed[m-2] == 0){
-            flowerbed[m-1] = 1;
-            n--;
-             if(n == 0)return true;
-        }
-  
-            return false;
+        if(n==0)return true;
+        return false;
 
         
     }
