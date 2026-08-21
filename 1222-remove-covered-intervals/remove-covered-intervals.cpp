c800e693371f0dac794dabe426af6453256d5a8f
Aryan Kumar <aryanempire168@gmail.com>
2026-08-21 18:01:44 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (97.17%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -11,18 +11,23 @@ public:
         sort(arr.begin(), arr.end(), cmp);
 
         int end = arr[0][1];
-        int cnt = 0;
+
+        // arr[0] is remaining
+        int cnt = 1;
 
         for(int i = 1; i < arr.size(); i++) {
 
             if(end >= arr[i][1]) {
-                cnt++;
+                // current interval is covered
+                continue;
             }
             else {
+                // current interval remains
+                cnt++;
                 end = arr[i][1];
             }
         }
 
-        return arr.size() - cnt;
+        return cnt;
     }
 };
