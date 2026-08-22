ba51351d4fb5f274df83c8b01b6d452adace7917
Aryan Kumar <aryanempire168@gmail.com>
2026-08-22 13:10:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (17.35%) - LeetSync

---
+++ 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin() , arr.end());
+        int i = 0;
+        int j = i+1;
+        vector<int>ans;
+        while(j < n){
+            ans.push_back(arr[j] - arr[i]);
+            i++;
+            j++;
+        }
+        int val = ans[0];
+        for(int i = 1 ; i < ans.size() ; i++){
+            if(ans[i] != val){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
