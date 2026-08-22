4f7a842e6277a9ba4abdc7217ed785bfebdc9a4c
Aryan Kumar <aryanempire168@gmail.com>
2026-08-22 16:57:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (40.49%) - LeetSync

--- 228-summary-ranges/summary-ranges.cpp
+++ 228-summary-ranges/summary-ranges.cpp
@@ -2,32 +2,31 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& arr) {
         int n = arr.size();
+        if(n == 0) return {};
         vector<string>ans;
-        if(n == 0)return ans;
-        int start = arr[0];
-
-
+        int last = arr[0];
+        int prev = last;
         for(int i = 1 ; i < n; i++){
-            if(arr[i] != arr[i-1]+1){
-                if(start == arr[i-1]){
-                    ans.push_back(to_string(start));
-                }else{
-                    ans.push_back(to_string(start) + "->" + to_string(arr[i-1]));
+            if(arr[i] != arr[i-1] +1){
+                if(last == arr[i-1]){
+                    ans.push_back(to_string(last));
                 }
-                start = arr[i];
+                else{
+                    ans.push_back(to_string(last) + "->" + to_string(arr[i-1]));
+                }
+                last = arr[i];
             }
+            
 
         }
+        if(last == arr[n-1]){
+            ans.push_back(to_string(last));
 
-        if(start == arr[n-1]){
-            ans.push_back(to_string(arr[n-1]));
-        }else{
-            ans.push_back(to_string(start)+ "->"+ to_string(arr[n-1]));
         }
-
+        else{
+            ans.push_back(to_string(last) + "->" + to_string(arr[n-1]));
+        }
         return ans;
-
-        
         
     }
 };
