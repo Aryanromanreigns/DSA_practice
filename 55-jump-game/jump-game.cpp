f2573c0d385cdcef7f09df40a7c210f11be5599f
Aryan Kumar <aryanempire168@gmail.com>
2026-08-22 21:03:15 +0530

    Time: 0 ms (100.00%) | Memory: 52.5 MB (25.87%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1)return true;
         int maxi = 0;
+
         for(int i = 0 ; i < n ; i++){
             if(i > maxi)return false;
-            maxi = max(maxi , i+nums[i]);
+            maxi = max(maxi , nums[i] + i);
         }
         return true;
         
