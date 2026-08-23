e1befb0352cb2c09540a70fd7c872f55d7620537
Aryan Kumar <aryanempire168@gmail.com>
2026-08-23 22:53:25 +0530

    Time: 3 ms (74.28%) | Memory: 17 MB (73.35%) - LeetSync

---
+++ 179-largest-number/largest-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    static bool cmp(string &a , string &b){
+        return a+b > b+a;
+    }
+    string largestNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<string>arr;
+        for(int i = 0 ; i < n ; i++){
+            arr.push_back(to_string(nums[i]));
+        }
+        sort(arr.begin() , arr.end() , cmp);
+
+        string ans = "";
+
+        for(int i = 0 ; i < arr.size() ; i++){
+            ans += arr[i];
+        }
+        if(ans[0] == '0')return "0";
+        return ans;
+
+
+
+
+        
+    }
+};
