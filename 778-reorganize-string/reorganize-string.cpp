94056c363a3ffee5f25619a0debef8f625cf60db
Aryan Kumar <aryanempire168@gmail.com>
2026-08-23 16:13:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (79.74%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,64 +1,57 @@
 class Solution {
 public:
     struct cmp{
-        bool operator()(pair<int , int>&a , pair<int ,int>&b){
-            if(a.first == b.first){
-                return a.second < b.second;
-            }
-            return a.first < b.first;
+     bool operator()(pair<int , char>&a , pair<int , char>&b){
+        if(a.first == b.first){
+            return a.second < b.second;
         }
+        return a.first < b.first;
+     }
     };
     string reorganizeString(string s) {
         int n = s.size();
+        string ans ;
         unordered_map<char , int>mpp;
-        priority_queue<pair<int , int > , vector<pair<int , int >> , cmp>pq;
-
         for(int i = 0 ; i < n ; i++){
             mpp[s[i]]++;
         }
 
+        priority_queue<pair<int , char> , vector<pair<int , char >> , cmp>pq;
         for(auto it : mpp){
-            int cnt = it.second;
-            int val = it.first;
-
-            pq.push({cnt , val});
+            pq.push({it.second , it.first});
         }
 
-        string ans;
+        char prev = '#';
         while(!pq.empty()){
-            auto top = pq.top();
-            int freq = top.first;
-            char ch = top.second;
+            int cnt = pq.top().first;
+            char val = pq.top().second;
             pq.pop();
 
-            if(ans.empty() || ans.back() != ch){
-                ans += ch;
-                freq--;
-                if(freq > 0){
-                    pq.push({freq , ch});
+            if(ans.empty() || ans.back() !=val){
+                ans += val;
+                cnt--;
+                if(cnt > 0){
+                    pq.push({cnt , val});
                 }
-
             }
             else{
-                if(pq.empty()){
-                    return "";
-                }
+                if(pq.empty())return "";
                 auto pp = pq.top();
                 pq.pop();
                 ans += pp.second;
                 pp.first--;
-
                 if(pp.first > 0){
                     pq.push(pp);
                 }
-                pq.push({freq , ch});
 
+                pq.push({cnt , val});
             }
+
+
+
+
         }
-
         return ans;
-
-
         
     }
 };
