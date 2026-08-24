8d20ca383680eb75aa7410c9fef7a71c0b4b96ca
Aryan Kumar <aryanempire168@gmail.com>
2026-08-24 21:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (20.99%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int func(int n , int ind , vector<int>&dp ){
-        if(ind == n){
-            return 1;
-        }
-        if(ind > n){
-            return 0;
-        }
-
+    int func(int n , int ind , vector<int>&dp){
+        if(ind == n)return 1;
+        if(ind > n)return 0;
         if(dp[ind] != -1)return dp[ind];
+        int one = func(n , ind+1 , dp);
+        int two = func(n , ind + 2 , dp);
 
-
-        int chone = func(n , ind+1 , dp);
-        int chtwo = func(n , ind+2 , dp);
-        return dp[ind] = chone + chtwo;
+        return dp[ind] = one + two;
     }
-
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return func(n ,0 , dp);
+        vector<int>dp(n+1 , -1);
+        return func(n , 0 , dp);
         
     }
 };
