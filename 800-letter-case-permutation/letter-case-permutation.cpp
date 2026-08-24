af3e4ca4f2c7abaf6df129b336ce7c247e7cff6e
Aryan Kumar <aryanempire168@gmail.com>
2026-08-24 15:46:35 +0530

    Time: 4 ms (32.60%) | Memory: 15.3 MB (37.89%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.cpp
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    void func(string s , vector<string>&ans , int ind , string str){
-        int n = s.size();
-
-        if(ind == n){
+    void func(int ind , string s , vector<string>&ans,
+        string str)
+    {
+        if(ind == s.size()){
             ans.push_back(str);
-            return ;
+            return;
         }
+
         if(isalpha(s[ind])){
-        func(s , ans , ind+1 , str + (char)toupper(s[ind]));
-        func(s , ans , ind+1 , str + (char)tolower(s[ind]));
-
+            func(ind + 1 , s , ans , str + (char)toupper(s[ind]));
+            func(ind + 1 , s , ans , str + (char)tolower(s[ind]));
         }
-        else
-        func(s , ans , ind+1 , str+s[ind]);
-
+        else{
+            func(ind + 1 , s , ans , str + s[ind]);
+        }
         
     }
     vector<string> letterCasePermutation(string s) {
+        int n = s.size();
         vector<string>ans;
         string str;
-        func(s , ans , 0 , str);
+        func(0 , s , ans , str);
         return ans;
-        
         
     }
 };
