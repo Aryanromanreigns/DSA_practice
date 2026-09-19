cc2d242b048c7b28e8c496603a5d8480692ebddd
Aryan Kumar <aryanempire168@gmail.com>
2026-09-19 17:52:47 +0530

    Time: 25 ms (20.22%) | Memory: 14.8 MB (21.65%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
+
         int n = s.size();
         int m = p.size();
 
-        unordered_map<char , int>need;
-        for(int i = 0 ; i <m; i++){
-            need[p[i]]++;
+        unordered_map<char , int>mpp;
+        unordered_map<char , int>mpps;
+
+        for(int i = 0 ; i < m ; i++){
+            mpp[p[i]]++;
         }
 
-        unordered_map<char , int>window;
         int left = 0;
         int right = 0;
+
         vector<int>ans;
 
         while(right < n){
-            window[s[right]]++;
+            mpps[s[right]]++;
 
-            if(right - left + 1 > p.size()){
-                window[s[left]]--;
+            if(right - left + 1 > m){
+                mpps[s[left]]--;
 
-                if(window[s[left]] == 0){
-                    window.erase(s[left]);
+                if(mpps[s[left]] == 0){
+                    mpps.erase(s[left]);
                 }
                 left++;
             }
 
-            if(right - left + 1 == p.size()){
-                if(window == need){
+            if(right - left + 1 == m){
+                if(mpps == mpp){
                     ans.push_back(left);
                 }
             }
+
             right++;
+
+
         }
         return ans;
 
