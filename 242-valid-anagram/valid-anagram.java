eb91d55ea5ae48a63bc5a09020bca3da0ca6041c
Aryan Kumar <aryanempire168@gmail.com>
2026-09-19 17:11:02 +0530

    Time: 21 ms (7.31%) | Memory: 46.8 MB (8.95%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,26 +4,27 @@ class Solution {
         int m = t.length();
 
         if(n != m)return false;
-        HashMap<Character , Integer>mpp = new HashMap<>();
 
+        HashMap<Character , Integer>mpp = new HashMap<>();
         for(int i = 0 ; i < n ; i++){
             mpp.put(s.charAt(i) , mpp.getOrDefault(s.charAt(i),0)+1);
         }
-        
+
         for(int i = 0 ; i < m ; i++){
             if(mpp.containsKey(t.charAt(i))){
                 mpp.put(t.charAt(i) , mpp.get(t.charAt(i))-1);
 
+
                 if(mpp.get(t.charAt(i)) == 0){
                     mpp.remove(t.charAt(i));
                 }
-            }
-            else{
+
+            }else{
                 return false;
             }
-
         }
         return true;
+
         
     }
 }
